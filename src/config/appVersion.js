68c4d0a420e0035b5c5dea5e07100ebabd08414d
shouq.alameri <shouq.alameri@shouqalameris-MacBook-Air.local>
2026-04-27 22:56:32 +0400

    Fixing

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,7 +1,7 @@
 module.exports = {
   ios: {
     latestVersion: "1.0.3",
-    minRequiredVersion: "1.0.3",
+    minRequiredVersion: "1.0.2",
     storeUrl: "https://apps.apple.com/ae/app/glowee-app/id6761380382",
   },
 
